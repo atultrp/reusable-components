ed1d7c17bb7e0ebe9d55aadabcdd4da815a64a89
Atul Tripathi <atul31815@gmail.com>
2023-03-29 16:35:49 +0530

    cleanup

--- components/Hero/Hero7.js
+++ components/Hero/Hero7.js
@@ -3,28 +3,28 @@ import React from 'react'
 const Hero7 = () => {
   return (
     <>
-      <div class="flex flex-wrap md items-center h-screen">
-        <div class="bg-white w-full md:w-1/2 h-screen">
-          <div class="mx-32">
-            <h1 class="text-6xl font-bold mt-16">Tokyo</h1>
+      <div className="flex flex-wrap md items-center h-screen">
+        <div className="bg-white w-full md:w-1/2 h-screen">
+          <div className="mx-32">
+            <h1 className="text-6xl font-bold mt-16">Tokyo</h1>
 
-            <div class="flex mt-16 font-light text-gray-500">
-              <div class="pr-4">
-                <span class="uppercase">Country</span>
-                <p class="text-2xl text-gray-900 font-semibold pt-2">Japan</p>
+            <div className="flex mt-16 font-light text-gray-500">
+              <div className="pr-4">
+                <span className="uppercase">Country</span>
+                <p className="text-2xl text-gray-900 font-semibold pt-2">Japan</p>
               </div>
-              <div class="pr-4">
-                <span class="uppercase">Region</span>
-                <p class="text-2xl text-gray-900 font-semibold pt-2">Kanto</p>
+              <div className="pr-4">
+                <span className="uppercase">Region</span>
+                <p className="text-2xl text-gray-900 font-semibold pt-2">Kanto</p>
               </div>
-              <div class="pr-4">
-                <span class="uppercase">island</span>
-                <p class="text-2xl text-gray-900 font-semibold pt-2">Honshu</p>
+              <div className="pr-4">
+                <span className="uppercase">island</span>
+                <p className="text-2xl text-gray-900 font-semibold pt-2">Honshu</p>
               </div>
             </div>
 
             <div
-              class="description w-full sm: md:w-2/3 mt-16 text-gray-500 text-sm"
+              className="description w-full sm: md:w-2/3 mt-16 text-gray-500 text-sm"
             >
               Tokyo, Japan’s busy capital, mixes the ultramodern and the
               traditional, from neon-lit skyscrapers to historic temples. The
@@ -33,15 +33,15 @@ const Hero7 = () => {
               gardens
             </div>
 
-            <button class="uppercase mt-5 text-sm font-semibold hover:underline">
+            <button className="uppercase mt-5 text-sm font-semibold hover:underline">
               read more
             </button>
           </div>
         </div>
-        <div class="bg-red-600 w-full md:w-1/2 h-screen">
+        <div className="bg-red-600 w-full md:w-1/2 h-screen">
           <img
             src="https://source.unsplash.com/7H77FWkK_x4/1600x900"
-            class="h-screen w-full"
+            className="h-screen w-full"
             alt=""
           />
         </div>
