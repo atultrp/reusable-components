05a45a19ce064f4bc46809b0619c7d4b55b38758
Atul Tripathi <atul31815@gmail.com>
2023-04-13 15:29:56 +0530

    footer 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Footer1 from 'components/Footers/Footer1'
 import Navbar from 'components/Navbars/Navbar'
 import Navbar1 from 'components/Navbars/Navbar1'
 import Navbar2 from 'components/Navbars/Navbar2'
@@ -8,12 +9,20 @@ import Navbar5 from 'components/Navbars/Navbar5'
 export default function Home() {
   return (
     <>
+
+      {/* NAVBAR SECTION  */}
+
       {/* <Navbar /> */}
       {/* <Navbar1 /> */}
       {/* <Navbar2 /> */}
       {/* <Navbar3 /> */}
       {/* <Navbar4 /> */}
       {/* <Navbar5 /> */}
+
+
+      {/* FOOTER SECTION */}
+      
+      {/* <Footer1 /> */}
     </>
   )
 }
