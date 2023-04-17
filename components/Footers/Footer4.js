c5c82cb2c6d46f092b573584de890302059bbc0a
Atul Tripathi <atul31815@gmail.com>
2023-04-17 15:43:08 +0530

    cleanup

--- components/Footers/Footer4.js
+++ components/Footers/Footer4.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Footer4 = () => {
   return (
     <div>
-      <footer aria-label="Site Footer" class="bg-white">
-        <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-          <div class="lg:flex lg:items-start lg:gap-8">
-            <div class="text-teal-600">
+      <footer aria-label="Site Footer" className="bg-white">
+        <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+          <div className="lg:flex lg:items-start lg:gap-8">
+            <div className="text-teal-600">
               <svg
-                class="h-8"
+                className="h-8"
                 viewBox="0 0 28 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -21,37 +21,37 @@ const Footer4 = () => {
             </div>
 
             <div
-              class="mt-8 grid grid-cols-2 gap-8 lg:mt-0 lg:grid-cols-5 lg:gap-y-16"
+              className="mt-8 grid grid-cols-2 gap-8 lg:mt-0 lg:grid-cols-5 lg:gap-y-16"
             >
-              <div class="col-span-2">
+              <div className="col-span-2">
                 <div>
-                  <h2 class="text-2xl font-bold text-gray-900">
+                  <h2 className="text-2xl font-bold text-gray-900">
                     Get the latest news!
                   </h2>
 
-                  <p class="mt-4 text-gray-500">
+                  <p className="mt-4 text-gray-500">
                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse non
                     cupiditate quae nam molestias.
                   </p>
                 </div>
               </div>
 
-              <div class="col-span-2 lg:col-span-3 lg:flex lg:items-end">
-                <form class="w-full">
-                  <label for="UserEmail" class="sr-only"> Email </label>
+              <div className="col-span-2 lg:col-span-3 lg:flex lg:items-end">
+                <form className="w-full">
+                  <label for="UserEmail" className="sr-only"> Email </label>
 
                   <div
-                    class="border border-gray-100 p-2 focus-within:ring sm:flex sm:items-center sm:gap-4"
+                    className="border border-gray-100 p-2 focus-within:ring sm:flex sm:items-center sm:gap-4"
                   >
                     <input
                       type="email"
                       id="UserEmail"
                       placeholder="john@rhcp.com"
-                      class="w-full border-none focus:border-transparent focus:ring-transparent sm:text-sm"
+                      className="w-full border-none focus:border-transparent focus:ring-transparent sm:text-sm"
                     />
 
                     <button
-                      class="mt-1 w-full bg-teal-500 px-6 py-3 text-sm font-bold uppercase tracking-wide text-white transition-none hover:bg-teal-600 sm:mt-0 sm:w-auto sm:shrink-0"
+                      className="mt-1 w-full bg-teal-500 px-6 py-3 text-sm font-bold uppercase tracking-wide text-white transition-none hover:bg-teal-600 sm:mt-0 sm:w-auto sm:shrink-0"
                     >
                       Sign Up
                     </button>
@@ -59,37 +59,37 @@ const Footer4 = () => {
                 </form>
               </div>
 
-              <div class="col-span-2 sm:col-span-1">
-                <p class="font-medium text-gray-900">Services</p>
+              <div className="col-span-2 sm:col-span-1">
+                <p className="font-medium text-gray-900">Services</p>
 
-                <nav aria-label="Footer Navigation - Services" class="mt-6">
-                  <ul class="space-y-4 text-sm">
+                <nav aria-label="Footer Navigation - Services" className="mt-6">
+                  <ul className="space-y-4 text-sm">
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         1on1 Coaching
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         Company Review
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         Accounts Review
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         HR Consulting
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         SEO Optimisation
                       </a>
                     </li>
@@ -97,25 +97,25 @@ const Footer4 = () => {
                 </nav>
               </div>
 
-              <div class="col-span-2 sm:col-span-1">
-                <p class="font-medium text-gray-900">Company</p>
+              <div className="col-span-2 sm:col-span-1">
+                <p className="font-medium text-gray-900">Company</p>
 
-                <nav aria-label="Footer Navigation - Company" class="mt-6">
-                  <ul class="space-y-4 text-sm">
+                <nav aria-label="Footer Navigation - Company" className="mt-6">
+                  <ul className="space-y-4 text-sm">
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         About
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         Meet the Team
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         Accounts Review
                       </a>
                     </li>
@@ -123,25 +123,25 @@ const Footer4 = () => {
                 </nav>
               </div>
 
-              <div class="col-span-2 sm:col-span-1">
-                <p class="font-medium text-gray-900">Helpful Links</p>
+              <div className="col-span-2 sm:col-span-1">
+                <p className="font-medium text-gray-900">Helpful Links</p>
 
-                <nav aria-label="Footer Navigation - Company" class="mt-6">
-                  <ul class="space-y-4 text-sm">
+                <nav aria-label="Footer Navigation - Company" className="mt-6">
+                  <ul className="space-y-4 text-sm">
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         Contact
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         FAQs
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         Live Chat
                       </a>
                     </li>
@@ -149,31 +149,31 @@ const Footer4 = () => {
                 </nav>
               </div>
 
-              <div class="col-span-2 sm:col-span-1">
-                <p class="font-medium text-gray-900">Legal</p>
+              <div className="col-span-2 sm:col-span-1">
+                <p className="font-medium text-gray-900">Legal</p>
 
-                <nav aria-label="Footer Navigation - Legal" class="mt-6">
-                  <ul class="space-y-4 text-sm">
+                <nav aria-label="Footer Navigation - Legal" className="mt-6">
+                  <ul className="space-y-4 text-sm">
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         Accessibility
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         Returns Policy
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         Refund Policy
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         Hiring Statistics
                       </a>
                     </li>
@@ -181,19 +181,19 @@ const Footer4 = () => {
                 </nav>
               </div>
 
-              <div class="col-span-2 sm:col-span-1">
-                <p class="font-medium text-gray-900">Downloads</p>
+              <div className="col-span-2 sm:col-span-1">
+                <p className="font-medium text-gray-900">Downloads</p>
 
-                <nav aria-label="Footer Navigation - Downloads" class="mt-6">
-                  <ul class="space-y-4 text-sm">
+                <nav aria-label="Footer Navigation - Downloads" className="mt-6">
+                  <ul className="space-y-4 text-sm">
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         Marketing Calendar
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         SEO Infographics
                       </a>
                     </li>
@@ -202,19 +202,19 @@ const Footer4 = () => {
               </div>
 
               <ul
-                class="col-span-2 flex justify-start gap-6 lg:col-span-5 lg:justify-end"
+                className="col-span-2 flex justify-start gap-6 lg:col-span-5 lg:justify-end"
               >
                 <li>
                   <a
                     href="/"
                     rel="noreferrer"
                     target="_blank"
-                    class="text-gray-700 transition hover:opacity-75"
+                    className="text-gray-700 transition hover:opacity-75"
                   >
-                    <span class="sr-only">Facebook</span>
+                    <span className="sr-only">Facebook</span>
 
                     <svg
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                       fill="currentColor"
                       viewBox="0 0 24 24"
                       aria-hidden="true"
@@ -233,12 +233,12 @@ const Footer4 = () => {
                     href="/"
                     rel="noreferrer"
                     target="_blank"
-                    class="text-gray-700 transition hover:opacity-75"
+                    className="text-gray-700 transition hover:opacity-75"
                   >
-                    <span class="sr-only">Instagram</span>
+                    <span className="sr-only">Instagram</span>
 
                     <svg
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                       fill="currentColor"
                       viewBox="0 0 24 24"
                       aria-hidden="true"
@@ -257,12 +257,12 @@ const Footer4 = () => {
                     href="/"
                     rel="noreferrer"
                     target="_blank"
-                    class="text-gray-700 transition hover:opacity-75"
+                    className="text-gray-700 transition hover:opacity-75"
                   >
-                    <span class="sr-only">Twitter</span>
+                    <span className="sr-only">Twitter</span>
 
                     <svg
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                       fill="currentColor"
                       viewBox="0 0 24 24"
                       aria-hidden="true"
@@ -279,12 +279,12 @@ const Footer4 = () => {
                     href="/"
                     rel="noreferrer"
                     target="_blank"
-                    class="text-gray-700 transition hover:opacity-75"
+                    className="text-gray-700 transition hover:opacity-75"
                   >
-                    <span class="sr-only">GitHub</span>
+                    <span className="sr-only">GitHub</span>
 
                     <svg
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                       fill="currentColor"
                       viewBox="0 0 24 24"
                       aria-hidden="true"
@@ -303,12 +303,12 @@ const Footer4 = () => {
                     href="/"
                     rel="noreferrer"
                     target="_blank"
-                    class="text-gray-700 transition hover:opacity-75"
+                    className="text-gray-700 transition hover:opacity-75"
                   >
-                    <span class="sr-only">Dribbble</span>
+                    <span className="sr-only">Dribbble</span>
 
                     <svg
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                       fill="currentColor"
                       viewBox="0 0 24 24"
                       aria-hidden="true"
@@ -325,28 +325,28 @@ const Footer4 = () => {
             </div>
           </div>
 
-          <div class="mt-8 border-t border-gray-100 pt-8">
-            <div class="sm:flex sm:justify-between">
-              <p class="text-xs text-gray-500">
+          <div className="mt-8 border-t border-gray-100 pt-8">
+            <div className="sm:flex sm:justify-between">
+              <p className="text-xs text-gray-500">
                 &copy; 2022. Company Name. All rights reserved.
               </p>
 
-              <nav aria-label="Footer Navigation - Support" class="mt-8 sm:mt-0">
-                <ul class="flex flex-wrap justify-start gap-4 text-xs lg:justify-end">
+              <nav aria-label="Footer Navigation - Support" className="mt-8 sm:mt-0">
+                <ul className="flex flex-wrap justify-start gap-4 text-xs lg:justify-end">
                   <li>
-                    <a href="#" class="text-gray-500 transition hover:opacity-75">
+                    <a href="#" className="text-gray-500 transition hover:opacity-75">
                       Terms & Conditions
                     </a>
                   </li>
 
                   <li>
-                    <a href="#" class="text-gray-500 transition hover:opacity-75">
+                    <a href="#" className="text-gray-500 transition hover:opacity-75">
                       Privacy Policy
                     </a>
                   </li>
 
                   <li>
-                    <a href="#" class="text-gray-500 transition hover:opacity-75">
+                    <a href="#" className="text-gray-500 transition hover:opacity-75">
                       Cookies
                     </a>
                   </li>
--- components/Footers/Footer5.js
+++ components/Footers/Footer5.js
@@ -3,48 +3,48 @@ import React from 'react'
 const Footer5 = () => {
   return (
     <div>
-      <footer aria-label="Site Footer" class="bg-white lg:grid lg:grid-cols-5">
-        <div class="relative block h-32 lg:col-span-2 lg:h-full">
+      <footer aria-label="Site Footer" className="bg-white lg:grid lg:grid-cols-5">
+        <div className="relative block h-32 lg:col-span-2 lg:h-full">
           <img
             src="https://images.unsplash.com/photo-1642370324100-324b21fab3a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80"
             alt=""
-            class="absolute inset-0 object-cover w-full h-full"
+            className="absolute inset-0 object-cover w-full h-full"
           />
         </div>
 
-        <div class="px-4 py-16 sm:px-6 lg:col-span-3 lg:px-8">
-          <div class="grid grid-cols-1 gap-8 sm:grid-cols-2">
+        <div className="px-4 py-16 sm:px-6 lg:col-span-3 lg:px-8">
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
             <div>
               <p>
-                <span class="text-xs tracking-wide text-gray-500 uppercase">
+                <span className="text-xs tracking-wide text-gray-500 uppercase">
                   Call us
                 </span>
 
                 <a
                   href="#"
-                  class="block text-2xl font-medium text-gray-900 hover:opacity-75 sm:text-3xl"
+                  className="block text-2xl font-medium text-gray-900 hover:opacity-75 sm:text-3xl"
                 >
                   0123456789
                 </a>
               </p>
 
-              <ul class="mt-8 space-y-1 text-sm text-gray-700">
+              <ul className="mt-8 space-y-1 text-sm text-gray-700">
                 <li>Monday to Friday: 10am - 5pm</li>
                 <li>Weekend: 10am - 3pm</li>
               </ul>
 
-              <ul class="flex gap-6 mt-8">
+              <ul className="flex gap-6 mt-8">
                 <li>
                   <a
                     href="/"
                     rel="noreferrer"
                     target="_blank"
-                    class="text-gray-700 transition hover:opacity-75"
+                    className="text-gray-700 transition hover:opacity-75"
                   >
-                    <span class="sr-only">Facebook</span>
+                    <span className="sr-only">Facebook</span>
 
                     <svg
-                      class="w-6 h-6"
+                      className="w-6 h-6"
                       fill="currentColor"
                       viewBox="0 0 24 24"
                       aria-hidden="true"
@@ -63,12 +63,12 @@ const Footer5 = () => {
                     href="/"
                     rel="noreferrer"
                     target="_blank"
-                    class="text-gray-700 transition hover:opacity-75"
+                    className="text-gray-700 transition hover:opacity-75"
                   >
-                    <span class="sr-only">Instagram</span>
+                    <span className="sr-only">Instagram</span>
 
                     <svg
-                      class="w-6 h-6"
+                      className="w-6 h-6"
                       fill="currentColor"
                       viewBox="0 0 24 24"
                       aria-hidden="true"
@@ -87,12 +87,12 @@ const Footer5 = () => {
                     href="/"
                     rel="noreferrer"
                     target="_blank"
-                    class="text-gray-700 transition hover:opacity-75"
+                    className="text-gray-700 transition hover:opacity-75"
                   >
-                    <span class="sr-only">Twitter</span>
+                    <span className="sr-only">Twitter</span>
 
                     <svg
-                      class="w-6 h-6"
+                      className="w-6 h-6"
                       fill="currentColor"
                       viewBox="0 0 24 24"
                       aria-hidden="true"
@@ -109,12 +109,12 @@ const Footer5 = () => {
                     href="/"
                     rel="noreferrer"
                     target="_blank"
-                    class="text-gray-700 transition hover:opacity-75"
+                    className="text-gray-700 transition hover:opacity-75"
                   >
-                    <span class="sr-only">GitHub</span>
+                    <span className="sr-only">GitHub</span>
 
                     <svg
-                      class="w-6 h-6"
+                      className="w-6 h-6"
                       fill="currentColor"
                       viewBox="0 0 24 24"
                       aria-hidden="true"
@@ -133,12 +133,12 @@ const Footer5 = () => {
                     href="/"
                     rel="noreferrer"
                     target="_blank"
-                    class="text-gray-700 transition hover:opacity-75"
+                    className="text-gray-700 transition hover:opacity-75"
                   >
-                    <span class="sr-only">Dribbble</span>
+                    <span className="sr-only">Dribbble</span>
 
                     <svg
-                      class="w-6 h-6"
+                      className="w-6 h-6"
                       fill="currentColor"
                       viewBox="0 0 24 24"
                       aria-hidden="true"
@@ -154,38 +154,38 @@ const Footer5 = () => {
               </ul>
             </div>
 
-            <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
               <div>
-                <p class="font-medium text-gray-900">Services</p>
+                <p className="font-medium text-gray-900">Services</p>
 
-                <nav aria-label="Footer Navigation - Services" class="mt-6">
-                  <ul class="space-y-4 text-sm">
+                <nav aria-label="Footer Navigation - Services" className="mt-6">
+                  <ul className="space-y-4 text-sm">
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         1on1 Coaching
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         Company Review
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         Accounts Review
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         HR Consulting
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         SEO Optimisation
                       </a>
                     </li>
@@ -194,24 +194,24 @@ const Footer5 = () => {
               </div>
 
               <div>
-                <p class="font-medium text-gray-900">Company</p>
+                <p className="font-medium text-gray-900">Company</p>
 
-                <nav aria-label="Footer Navigation - Company" class="mt-6">
-                  <ul class="space-y-4 text-sm">
+                <nav aria-label="Footer Navigation - Company" className="mt-6">
+                  <ul className="space-y-4 text-sm">
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         About
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         Meet the Team
                       </a>
                     </li>
 
                     <li>
-                      <a href="#" class="text-gray-700 transition hover:opacity-75">
+                      <a href="#" className="text-gray-700 transition hover:opacity-75">
                         Accounts Review
                       </a>
                     </li>
@@ -221,31 +221,31 @@ const Footer5 = () => {
             </div>
           </div>
 
-          <div class="pt-12 mt-12 border-t border-gray-100">
-            <div class="sm:flex sm:items-center sm:justify-between">
+          <div className="pt-12 mt-12 border-t border-gray-100">
+            <div className="sm:flex sm:items-center sm:justify-between">
               <nav aria-label="Footer Navigation - Support">
-                <ul class="flex flex-wrap gap-4 text-xs">
+                <ul className="flex flex-wrap gap-4 text-xs">
                   <li>
-                    <a href="#" class="text-gray-500 transition hover:opacity-75">
+                    <a href="#" className="text-gray-500 transition hover:opacity-75">
                       Terms & Conditions
                     </a>
                   </li>
 
                   <li>
-                    <a href="#" class="text-gray-500 transition hover:opacity-75">
+                    <a href="#" className="text-gray-500 transition hover:opacity-75">
                       Privacy Policy
                     </a>
                   </li>
 
                   <li>
-                    <a href="#" class="text-gray-500 transition hover:opacity-75">
+                    <a href="#" className="text-gray-500 transition hover:opacity-75">
                       Cookies
                     </a>
                   </li>
                 </ul>
               </nav>
 
-              <p class="mt-8 text-xs text-gray-500 sm:mt-0">
+              <p className="mt-8 text-xs text-gray-500 sm:mt-0">
                 &copy; 2022. Company Name. All rights reserved.
               </p>
             </div>
