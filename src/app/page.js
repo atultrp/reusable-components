ac45670740d5337332330e321653c0e135c5001f
Atul Tripathi <atul31815@gmail.com>
2023-04-19 15:58:58 +0530

    hero1

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Footer2 from 'components/Footers/Footer2'
 import Footer3 from 'components/Footers/Footer3'
 import Footer4 from 'components/Footers/Footer4'
 import Footer5 from 'components/Footers/Footer5'
+import Hero1 from 'components/Hero/Hero1'
 import Navbar from 'components/Navbars/Navbar'
 import Navbar1 from 'components/Navbars/Navbar1'
 import Navbar2 from 'components/Navbars/Navbar2'
@@ -22,6 +23,11 @@ export default function Home() {
       {/* <Navbar3 /> */}
       {/* <Navbar4 /> */}
       {/* <Navbar5 /> */}
+
+
+      {/* HERO SECTION */}
+
+      {/* <Hero1 /> */}
 
 
       {/* FOOTER SECTION */}
